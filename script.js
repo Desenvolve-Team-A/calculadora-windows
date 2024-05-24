b768d3456b3d3234e462306e20f2302c1e236a43
Richard Nathan Domingos <richard.nathan@philips.com>
2024-05-24 10:04:53 -0300

    Base

---
+++ script.js
@@ -0,0 +1,31 @@
+function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b){
+    return a - b;
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
+
+function dividir(a, b){
+    return a / b;
+}
+
+function potencia(a){
+    return a * a;
+}
+
+function raiz(a){
+    return Math.sqrt(a);
+}
+
+function fracao(a){
+    return 1 / a;
+}
+
+function porcentagem(a, b){
+    return (a * (b / 100));
+}
